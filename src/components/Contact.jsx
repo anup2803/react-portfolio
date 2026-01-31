431aa48916220c411bac88069c9fc7b0dc7ddf23
Anup Kumal <anupbinu2803@gmail.com>
2026-01-31 08:06:47 +0545

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  FaDribbble,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+const Contact = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      viewport={{ once: false, amount: 0.2 }}
+      id="contact"
+      className="py-20 bg-dark-200"
+    >
+      <div className="container mx-auto px-6">
+        <h2 className="text-3xl font-bold text-center mb-3">
+          Get In
+          <span className="text-purple">Touch</span>
+        </h2>
+        <p className="text-gray-400 text-center max-w-2xl mx-auto mb-16">
+          Have a project in mind or want to collaborate? Let's talk!
+        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
+          {/* contact form */}
+          <div>
+            <form action="" className="space-y-6">
+              <div>
+                <label htmlFor="name" className="block text-gray-300 mb-2">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  className="w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-gray-300 mb-2">
+                  Your Email
+                </label>
+                <input
+                  type="text"
+                  className="w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none"
+                  name=""
+                  id=""
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-gray-300 mb-2">
+                  Your Message
+                </label>
+                <textarea
+                  type="text"
+                  className="w-full h-40 bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none"
+                  name=""
+                  id=""
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full px-6 py-3 bg-purple rounded-lg font-medium hover:bg-purple-700 transition duration-300"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
+          {/* contact info */}
+          <div className="space-y-8">
+            <div className="flex items-start  ">
+              <div className="text-purple text-2xl mr-4">
+                <FaMapMarkerAlt />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold mb-2">Locations</h3>
+                <p className="text-gray-400">Bharatpur-04,Chitwan,Nepal</p>
+              </div>
+            </div>
+            <div className="flex items-start">
+              <div className="text-purple text-2xl mr-4">
+                <FaEnvelope />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold mb-2">Email</h3>
+                <p className="text-gray-400">anup@example.com</p>
+              </div>
+            </div>
+            <div className="flex items-start">
+              <div className="text-purple text-2xl mr-4">
+                <FaPhone />
+              </div>
+              <div>
+                <h3 className="text-lg font-semibold mb-2">Phone</h3>
+                <p className="text-gray-400">9779845021942</p>
+              </div>
+            </div>
+            <div className="pt-4">
+              <h3 className="text-lg font-semibold mb-4">Follow Me</h3>
+              <div className="flex space-x-4">
+                <a
+                  href="#"
+                  className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-white hover:bg-gray-600 hover:text-white transition duration-300"
+                >
+                  <FaGithub />
+                </a>
+                <a
+                  href="#"
+                  className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-white hover:bg-blue-400 hover:text-white transition duration-300"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  href="#"
+                  className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-blue-400 hover:bg-blue-400 hover:text-white transition duration-300"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="#"
+                  className="w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-pink hover:bg-pink hover:text-white transition duration-300"
+                >
+                  <FaDribbble />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Navheader } from "../assets/assets";
+import { FaBars, FaXmark } from "react-icons/fa6";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <nav className="fixed w-full z-50 bg-dark-100/90 backdrop-blur-sm py-4 px-8 shadow-lg">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <a href="#home" className="text-3xl font-bold text-white">
+            Coder
+            <span className="text-purple"> Anup</span>
+            <div className="w-4 h-4 bg-purple rounded-full "></div>
+          </a>
+        </div>
+        <div className="hidden md:flex space-x-10">
+          {Navheader.map((item) => (
+            <a
+              href={`#${item.header}`}
+              className="relative text-white/80  transition duration-300 hover:text-purple group"
+              key={item.id}
+            >
+              <span className="pb-2">{item.title}</span>
+              <span className=" absolute left-0 bottom-1 w-0 h-0.5  bg-purple transition-all duration-300 group-hover:w-full  "></span>
+            </a>
+          ))}
+        </div>
+        {/* mobile button */}
+        <div className="md:hidden">
+          {showMenu ? (
+            <FaXmark
+              onClick={() => setShowMenu((prev) => !prev)}
+              className="text-2xl cursor-pointer"
+            />
+          ) : (
+            <FaBars
+              onClick={() => setShowMenu((prev) => !prev)}
+              className="text-2xl cursor-pointer"
+            />
+          )}
+        </div>
+      </div>
+      {/* mobile menu */}
+      {showMenu && (
+        <div className="md:hidden mt-4 bg-dark-300 h-screen rounded-lg p-4 flex flex-col space-y-4 text-center justify-center">
+          {Navheader.map((item) => (
+            <a
+              onClick={() => setShowMenu((prev) => !prev)}
+              href={`#${item.header}`}
+              className="relative text-white/80  transition duration-300 hover:text-purple group "
+              key={item.id}
+            >
+              <span className="pb-2">{item.header}</span>
+            </a>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
